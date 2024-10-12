b7a042b49ac5ac319f54285a2b1fbe22bd168c14
Moses Odjoji <nyeromani@gmail.com>
2024-10-12 09:10:27 +0000

    updated all files

--- models/internediate/marketing/int_dim_customer.sql
+++ models/internediate/marketing/int_dim_customer.sql
@@ -14,9 +14,10 @@ customer_orders as (
 
     select
         customer_id,
-        min(ordered_at) as first_order_date,
-        max(ordered_at) as most_recent_order_date,
-        count(ordered_at) as number_of_orders
+
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
 
     from orders
 
@@ -28,14 +29,15 @@ final as (
 
     select
         customers.customer_id,
-        substring(customers.name, 1, charindex(' ', customers.name)-1 ) as first_name,
-        substring(customers.name, charindex(' ', customers.name)+1, len(name) ) as last_name,
+        customers.first_name,
+        customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce (customer_orders.number_of_orders, 0) 
         as number_of_orders
 
     from customers
+
     left join customer_orders using (customer_id)
 
 )
